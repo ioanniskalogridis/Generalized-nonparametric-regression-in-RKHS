97ab3b4a05ee81cf3f4f758ac0377915447a7b88
Ioannis Kalogridis <ioannis.kalogridis@kuleuven.be>
2026-06-05 18:35:14 +0100

    Update Simulations.R

--- Simulations.R
+++ Simulations.R
@@ -46,7 +46,7 @@ generate_f0_2d <- function(beta, K = 25, p = 1.2) {
 # SIMULATION SETTINGS
 # ============================================================
 
-B <- 1000
+B <- 500
 n <- 200
 beta_levels <- c(0.5, 0.75, 1.0)
 error_types <- c("gaussian", "t2")
